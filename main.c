a339c8007ca595b48199f6faff9146bba0597542
jockebengtsson@outlook.com <jockebengtsson@outlook.com>
2025-03-24 15:52:16 +0100

    test

--- main.c
+++ main.c
@@ -5,6 +5,43 @@ int main()
 {
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
+    int dice_toss[100];
+    int dice_1 = 0;
+    int dice_2 = 0;
+    int dice_3 = 0;
+    int dice_4 = 0;
+    int dice_5 = 0;
+    int dice_6 = 0;
+    float sum = 0;
+    for(int i = 0; i < 100; i++){
+        dice_toss[i] = (rand() % 6) +1;
+        sum += dice_toss[i];
+        switch (dice_toss[i]){
+            case 1:
+                dice_1 += 1;
+            break;
+            case 2:
+                dice_2 += 1;
+            break;
+            case 3:
+                 dice_3 += 1;
+            break;
+            case 4:
+                 dice_4 += 1;
+            break;
+            case 5:
+                 dice_5 += 1;
+            break;
+            case 6:
+                 dice_6 += 1;
+            break;
+            default:
+            break;
+        }
+    }
+    printf("\n%d\n%d\n%d\n%d\n%d\n%d\n%.f\n", dice_1, dice_2, dice_3, dice_4, dice_5, dice_6, sum);
+    float average_value = sum / 100;
+    printf("%.1f", average_value);
 
    
     // Your code goes here
